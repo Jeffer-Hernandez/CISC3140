8cfb5de8e9cc722c620b28b1a91a9f2be0fe3908
Jeff <jeffhernandez@nypl.org>
2022-10-24 19:10:43 -0400

    set up the req structure for program

--- Exercise1.java
+++ Exercise1.java
@@ -1,23 +1,53 @@
 import java.time.*;
 import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class Exercise1 {  
    public static void main(String[] args)
-    {
-        LocalDate lab1= LocalDate.of(2022,9,28);
-        LocalDate lab2 = LocalDate.of(2022,10,26);
-        LocalDate lab3 = LocalDate.of(2022,11,23);
-        LocalDate lab4 = LocalDate.of(2022,12,07);
+    {   Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please choose from one of the following options: (enter an integer)\n");
+        System.out.println("1) Days from now until Lab1's Due Date\n");
+        System.out.println("2) Days from now until Lab2's Due Date\n");
+        System.out.println("3) Days from now until Lab3's Due Date\n");
+        System.out.println("4) Days from now until Lab4's Due Date\n");
+        System.out.println("5) Custom Date\n");
+
+        int ans = keyboard.nextInt();
+        
+        if (ans > 0 && ans < 5){
+            Period dif  = lab_days(ans);
+        }else if (ans == 6){
+            System.out.println("Please enter two dates in the MM/DD/YYYY format\n");
+            int date1 = keyboard.nextInt();
+            int date2 = keyboard.nextInt();
+            
+        }
+
+    }
+
+    public static Period lab_days(int ans)
+    {   
         LocalDate now = LocalDate.now();
- 
-        Period diff1 = Period.between(now, lab1);
-        Period diff2 = Period.between(now, lab2);
-        Period diff3 = Period.between(now, lab3);
-        Period diff4 = Period.between(now, lab4);
- 
-        System.out.printf("\nWe are %d months and %d days from the due date for lab1\n\n", diff1.getMonths(), diff1.getDays());
-        System.out.printf("\nWe are %d months and %d days from the due date for lab2\n\n", diff2.getMonths(), diff2.getDays());
-        System.out.printf("\nWe are %d months and %d days from the due date for lab3\n\n", diff3.getMonths(), diff3.getDays());
-        System.out.printf("\nWe are %d months and %d days from the due date for lab4\n\n", diff4.getMonths(), diff4.getDays());
+        switch (ans)
+        {
+            case 1:
+                LocalDate lab1= LocalDate.of(2022,9,28);
+                return Period.between(now, lab1);
+                
+            case 2:
+                LocalDate lab2 = LocalDate.of(2022,10,26);
+                return Period.between(now, lab2);
+            case 3:
+                LocalDate lab3 = LocalDate.of(2022,11,23);
+                return Period.between(now, lab3);
+            case 4:
+                LocalDate lab4 = LocalDate.of(2022,12,07);
+                return Period.between(now, lab4);
+            default:
+                System.out.println("I'm sorry, please choose a valid option.");
+                return Period.between(now, now);
+        }
     }
 }
